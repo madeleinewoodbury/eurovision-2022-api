b934c807e193b806878fe040c17ec78d29fe1cde
Madeleine Woodbury <mail@woodburycode.com>
2021-12-31 10:47:06 +0100

    Add access control methods to headers

--- server.js
+++ server.js
@@ -20,7 +20,14 @@ const app = express();
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, Content-Type, X-Auth-Token'
+  );
   next();
 });
 
